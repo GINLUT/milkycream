d99239f4934679d36f30214d9175152710d0e301
ginlut <ginalutfallah@gmail.com>
2022-03-20 21:39:21 -0300

    Proyecto final

--- js/main.js
+++ js/main.js
@@ -11,10 +11,10 @@ const contenedorProductos = document.querySelector(".productos"),
     totalCarrito = document.getElementById('total-carrito'),
     carritoAbrir = document.getElementById('botonCarrito'),
     carritoCerrar = document.getElementById('carritoCerrar'),
-    contadorCarrito = document.getElementById('contadorCarrito');
-
-const contenedorModal = document.getElementsByClassName('modal-contenedor')[0],
-    modalCarrito = document.getElementsByClassName('modal-carrito')[0];
+    contadorCarrito = document.getElementById('contadorCarrito'),
+    contenedorModal = document.getElementsByClassName('modal-contenedor')[0],
+    modalCarrito = document.getElementsByClassName('modal-carrito')[0],
+    contenedorCarrito = document.getElementById('contenedorCarrito');
 
 carritoAbrir.addEventListener('click', () => {
     contenedorModal.classList.toggle('modal-active')
@@ -29,11 +29,19 @@ contenedorModal.addEventListener('click', () => {
     carritoCerrar.click()
 })
 
+modalPago.addEventListener('click', () => {
+    irAPagar()
+})
+
+
+
+
 /*DECLARACIÓN DE FUNCIONES*/
 async function mostrarProductos() {
     const resp = await fetch("../productos.json")
     productos = await resp.json()
     listaProductos();
+    recuperarCarrito()
 }
 
 mostrarProductos()
@@ -44,7 +52,6 @@ function listaProductos() {
     productos.forEach(function(producto) {
         agregarCajaProducto(producto)
     })
-
 }
 
 //Agrega un producto del stock 
@@ -128,11 +135,12 @@ function agregarAlCarrito(codigoProducto) {
                         <p>Precio: $${precio}</p>
                         <p id="cantidad${codigo}">Cantidad:${cantidad}</p>
                         <button id="btnEliminar${codigo}" class="botonEliminar"><i class="fas fa-trash-alt"></i></button>
+
                         `
 
 
 
-        modalCarrito.appendChild(div)
+        contenedorCarrito.appendChild(div)
 
         // Se crea el boton para eliminar productos del carrito
 
@@ -148,12 +156,20 @@ function agregarAlCarrito(codigoProducto) {
                     cancelButtonColor: '#d33',
                     confirmButtonText: 'Eliminar'
                 }).then((result) => {
-                    console.log(result)
                     if (result.isConfirmed) {
-                        Swal.fire(
-                            'Eliminado',
-                            ` ${productoAgregar.nombre} ha sido eliminado del carrito`,
-                            'success')
+                        Toastify({
+                            text: `Producto eliminado del carrito`,
+                            duration: 3000,
+                            newWindow: true,
+                            close: true,
+                            gravity: "top",
+                            position: "right",
+                            stopOnFocus: true,
+                            style: {
+                                background: "#66d6e0",
+                            },
+                            onClick: function() {} // Callback after click
+                        }).showToast();
                         btnEliminar.parentElement.remove()
                         carrito = carrito.filter(item => item.codigo != productoAgregar.codigo)
                         actualizarCarrito()
@@ -185,16 +201,94 @@ function calcularTotal(...precios) { //SPREAD
     return precios.reduce((acc, n) => acc + n, 0)
 }
 
+//Recorre todos los productos y los agrega al HTML
+function irAPagar() {
+    if (carrito.length > 0) {
+        contenedorCarrito.innerHTML = "";
+        let div = document.createElement('div')
+        div.className = 'row g-0 cajaFormulario'
+        div.innerHTML = ` <form id="form" class="col">
+            <section class="seccionesPedidos">
+                <h2 class="subtituloPedidos">Datos personales</h2>
+                <div class="input-group mb-3">
+                    <label for="nombre_apellido" class="input-group-text">Nombre y Apellido:</label>
+                    <input type="text" class="form-control" name="nombre_apellido" required/>
+                </div>
+                <div class="input-group mb-3">
+                    <label for="telefono" class="input-group-text">Teléfono:</label>
+                    <input type="tel" class="form-control" name="telefono" required />
+                </div>
+                <div class="input-group mb-3">
+                    <label for="email" class="input-group-text">Correo electrónico:</label>
+                    <input type="email" class="form-control" name="email" required/>
+                </div>
+                <div class="input-group mb-3">
+                    <label for="direccion" class="input-group-text">Dirección:</label>
+                    <input type="text" class="form-control" name="direccion" required/>
+                </div>
+            </section>
+            <section class="seccionesPedidos" id="resumenProductos">
+                <h2 class="subtituloPedidos">Resumen del pedido</h2>
+            </section>
+            <section class="seccionesPedidos">
+                <h2 class="subtituloPedidos">Método de pago</h2>
+
+                <p class="descripcion"> <input type="radio" name="pago" >
+                    <img src="https://web.getjusto.com/images/payment-methods/credit_card_icons.svg" alt="credito" width="55"> Tarjeta de Crédito</p>
+
+                <p class="descripcion"><input type="radio" name="pago">
+                    <img src="https://web.getjusto.com/images/payment-methods/webpay.svg" alt="webpay" width="55"> Webpay</p>
+
+                <p class="descripcion"><input type="radio" name="pago"> <img src="https://web.getjusto.com/images/payment-methods/mach.svg" alt="webpay" width="55"> Mach</p>
+            </section>
+            <input type="submit" id="botonPagar" class="botonProductos" value="Confirmar pedido" />
+            </form>`
+        document.getElementById('modalPago').replaceWith(" ");
+        const totalCompra = document.getElementsByClassName('precioProducto')[0]
+        contenedorCarrito.appendChild(div)
+        document.getElementById('resumenProductos').appendChild(totalCompra)
+
+
+        const botonPagar = document.getElementById('form')
+
+        botonPagar.addEventListener('submit', () => {
+            carritoCerrar.click()
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Su pedido está siendo procesado',
+                text: '¡Muchas gracias por preferirnos!',
+                showConfirmButton: true,
+                timer: 4000
+            })
+            carrito = []
+            actualizarCarrito()
+            localStorage.setItem('carrito', JSON.stringify(carrito))
+            location.reload()
+        })
+    } else {
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: '¡Oops!',
+            text: 'Aún no has agregado nada al carrito',
+            showConfirmButton: true,
+            timer: 4000
+        })
+    }
+
+}
+
+
 
 //Recupera los productos seleccionados en el carrito si se reinicia la página
-// function recuperarCarrito() {
-//     let recuperarLS = JSON.parse(localStorage.getItem('carrito'))
-//     if (recuperarLS) {
-//         recuperarLS.forEach(element => {
-//             agregarAlCarrito(element.codigo)
-//         });
-//     }
 
-// }
+function recuperarCarrito() {
+    let recuperarLS = JSON.parse(localStorage.getItem('carrito'))
+    if (recuperarLS) {
+        recuperarLS.forEach(element => {
+            agregarAlCarrito(element.codigo)
+        });
+    }
 
-// recuperarCarrito()
+}
